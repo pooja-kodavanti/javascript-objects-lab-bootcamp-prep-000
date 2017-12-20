b073056cbc45fd16c161e7757447191b506573fd
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-20 19:39:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var recipes = {};
+
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  var obj = {'prop': 1 }
+  return Object.assign({}, obj, {'prop2': 2})
+}
+
+// const obj = {'prop': 1}
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  var obj = {'prop': 1 }
+  obj['prop2'] = 2;
+  return obj;
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var obj = { prop: 1 }
+  var newObj = Object.assign({}, obj)
+  delete newObj.prop
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  var obj = { prop: 1 }
+  delete obj.prop
+  return obj
+}
